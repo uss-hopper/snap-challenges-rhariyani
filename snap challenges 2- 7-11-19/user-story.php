5595dccc9e612cbdd251e2e1bec998a4c7107971
Rishita Hariyani <rhariyani@cnm.edu>
2019-07-11 09:04:35 -0600

    Submission

---
+++ snap challenges 2- 7-11-19/user-story.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>User Story</title>
+		<link rel="stylesheet" href="./styles.css">
+
+	</head>
+	<body>
+		<h1>User Story</h1>
+		<p>As a power user, I like to upload Image to site</p>
+		<div>
+			<img src= "EditDesign-button-Click.png"/>
+
+			<img src= "cb_imageblock_clickedit.png"/>
+
+		</div>
+
+		</body>
+</html>
