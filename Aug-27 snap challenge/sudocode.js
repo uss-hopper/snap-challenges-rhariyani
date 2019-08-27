67b7be030083c80457276ed8a4a4f3540b1b4ae4
Rishita Hariyani <rhariyani@cnm.edu>
2019-08-27 09:24:18 -0600

    sudo code

---
+++ Aug-27 snap challenge/sudocode.js
@@ -0,0 +1,41 @@
+// fizz buzz sudo code
+let int = number;
+for(i=1; i< x ;i++) {
+
+	if(number % 3 == 0 && number % 2 == 0 && number % 6) {
+		console.log("fizz bizz bazz")
+	} else if(number % 2 == 0) {
+		console.log("fizz.\n", number);
+	} else if(number % 3 == 0) {
+		console.log("bizz.\n", number);
+	} else {
+		console.log("Number ", number);
+	}
+}
+//sudo code
+numbers=[];
+count[];
+ foreach(number as number ){
+	count []++;
+}
+// count array
+function count(arr) {
+	var a = [];
+	var b = [];
+	var result;
+
+	arr.sort();
+	for(var i = 0; i < arr.length; i++) {
+		if(arr[i] !== result) {
+			a.push[arr[i]];
+			b.push(1);
+		} else {
+			b[b.length - 1]++;
+		}
+		result = arr[i];
+	}
+	return [a, b];
+}
+
+
+}
